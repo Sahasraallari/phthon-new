35d584e0bbe8686b109aefc72a5cea8ca5eb5926
Sahasraallari <mohan.velagala@gmail.com>
2025-08-01 17:34:11 +0530

    key

---
+++ yourself.py
@@ -0,0 +1,13 @@
+name =   input("please enter youre name")
+print("My name is",  name)
+
+
+age = int  (input  ("please enter youre age"))
+print("My age is" , age)
+
+
+
+School__name =  input("please enter youre  school name")
+print("My  school name is " , School__name )
+
+
