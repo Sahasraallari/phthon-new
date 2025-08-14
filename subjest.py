4bccca5b9682bce1734093a12af6ecd5235ed5a1
Sahasraallari <mohan.velagala@gmail.com>
2025-08-14 17:38:37 +0530

    tree

---
+++ subjest.py
@@ -0,0 +1,19 @@
+print("Enter marks ontained in 4 subjects:")
+maths = int(input("maths : " ))
+english = int(input("english : " ))
+science = int(input("science : " ))
+hindi = int(input("hindi : " ))
+
+sum = maths + english + science + hindi
+print("sum of math, english, science and hindi")
+
+perc = (sum/400)*100
+print(end= "Percentage Mark =")
+print(perc)
+
+
+
+
+
+
+
