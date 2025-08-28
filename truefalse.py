e8c72cc393a82436735a01bbc4e5d4415bff2e0e
Sahasraallari <mohan.velagala@gmail.com>
2025-08-28 17:45:56 +0530

    height

---
+++ truefalse.py
@@ -0,0 +1,21 @@
+a = 11
+b = 14
+c = 13
+
+if a == 10 and  b == 14 and c == 13:
+    print("All the valus are equal") 
+else :
+    print("All the values not equal")
+
+x = 18
+y = 11
+z = 14
+if x == 18 or y == 11 or z == 14:
+    print ("Only  one value is equal ")
+else:
+    print("Any value is not equal")
+
+    
+
+
+    
