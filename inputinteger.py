b907087267416da831450996a602c32d3e0a4bdb
Sahasraallari <mohan.velagala@gmail.com>
2025-08-30 15:16:56 +0530

    input

---
+++ inputinteger.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the numbr whose sum you want to find:"))
+sum = 0
+for i in range (1, n+1):
+    sum = sum+i
+    print ("\nSum = " , sum)
+
+
+
+
+
---
+++ reverse.py
@@ -0,0 +1,11 @@
+string = input("Please enter your own String:")
+string2 = ('')
+for i in string :
+    string2 = i + string2
+print("\n The Original String = " ,string )
+print("\n The Reverse String = " ,string2 )
+
+
+
+
+
