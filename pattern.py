f3b097f576c19f0715d41caa8b57daf0c9cef6d0
Sahasraallari <mohan.velagala@gmail.com>
2025-09-11 17:41:07 +0530

    rows

---
+++ pattern.py
@@ -0,0 +1,20 @@
+print("Half pyramid pattern of stars(*):")
+n = int(input("Enter the number of rows:"))
+for i in range (n):
+    for j in range(i+1):
+        print("* " , end="" )
+    print()
+
+print("Half Pyramid patterns of stars ")
+n = int(input("Enter the number of rows: "))
+for i in range(n, 0, -1):
+        for j in range(i):
+             print("* ", end="")
+        6
+        print()
+        
+   
+        
+
+
+
