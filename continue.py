905ad135591eeb6db87f9af5a6d492534595a826
Sahasraallari <mohan.velagala@gmail.com>
2025-10-09 17:28:19 +0530

    continue

---
+++ continue.py
@@ -0,0 +1,11 @@
+var = 18
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print("\nCurrent variablr Value:" , var)
+print("\n Good Bye!")
+
+
+
+ 
---
+++ return.py
@@ -0,0 +1,11 @@
+a = input("enter a word:")
+for i in a:
+    if (i == 'A'):
+        print("A is found")
+    else:
+        print("A not found")
+
+
+
+
+   
